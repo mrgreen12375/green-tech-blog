3b6236b7d4ecd658e3578a8f5fe25d8a56fa124b
Steven Green <ste2144105@gmail.com>
2022-12-26 23:32:11 -0700

    changed double quote to single

--- controllers/index.js
+++ controllers/index.js
@@ -3,7 +3,7 @@ const router = require('express').Router();
 
 const dashboardRoutes = require('./dashboardRoutes.js');
 const homeRoutes = require('./homeRoutes.js');
-const apiRoutes = require("./api")
+const apiRoutes = require('./api');
 
 router.use('/', homeRoutes);
 router.use('/dashboard', dashboardRoutes);
