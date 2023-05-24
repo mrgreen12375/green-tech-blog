14cc358d5e3bd6b1b48cc8ea6ee394596fa28a6c
Steven Green <ste2144105@gmail.com>
2023-05-24 00:56:05 -0700

    updated homeroute and added favicon

--- controllers/index.js
+++ controllers/index.js
@@ -2,7 +2,7 @@
 const router = require('express').Router();
 
 const dashboardRoutes = require('./dashboardRoutes.js');
-const homeRoutes = require('./homeRoutes.js');
+const homeRoutes = require('./homeroutes');
 const apiRoutes = require('./api');
 
 router.use('/', homeRoutes);
